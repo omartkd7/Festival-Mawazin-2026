74926f958846b17a012420fb92468b3b20d2fefe
omartkd7 <omartkd7@gmail.com>
2026-05-06 08:53:15 +0100

    this 2 update for me projict

---
+++ src/compnents/layout/Navbar.tsx
@@ -0,0 +1,37 @@
+
+import logo from "../../assets/logositeweb.png";
+
+
+
+
+
+export default  function Navbar () {
+
+    return (
+
+        <section>
+
+            <div >
+
+                <img src={logo} alt="Mawazin Logo" />
+                <div>
+                    <ul>
+
+                        <li><a href="#"> Home</a></li>
+                        <li><a href="#"> </a></li>
+                        <li><a href="#"> </a></li>
+
+
+                    </ul>
+
+
+                </div>
+
+            </div>
+
+        </section>
+       
+       
+
+    );
+}
